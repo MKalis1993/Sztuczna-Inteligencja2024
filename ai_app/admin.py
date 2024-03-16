67d8e28fd9217749a13fdb3a56aee7d848fe308d
Mateusz Kaliś 69731 <gax34625@student.gdynia.merito.pl>
2024-03-16 11:10:06 +0100

    stworzenie modelu i wykonanie migracji

--- ai_app/admin.py
+++ ai_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ImageElement
 
 # Register your models here.
+admin.site.register(ImageElement)
+
+
--- ai_app/models.py
+++ ai_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class ImageElement(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    content = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="mediaphoto", blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
 # Create your models here.
